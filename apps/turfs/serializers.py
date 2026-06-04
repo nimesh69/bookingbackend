109679a9d343632aaed0e3613a7524f63791499a
nimesh69 <nimeshstha79@gmail.com>
2026-06-04 22:42:12 +0545

    update api endpoint for turf and admin dashboard

--- apps/turfs/serializers.py
+++ apps/turfs/serializers.py
@@ -125,7 +125,7 @@ class VenueListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Venue
-        fields = ['id', 'name', 'location', 'status', 'cover_image', 'turfs_count', 'created_at']
+        fields = ['id', 'name', 'location', 'status', 'cover_image', 'turfs_count', 'created_at','owner']
         read_only_fields = ['id', 'created_at']
 
     @extend_schema_field(serializers.IntegerField())
